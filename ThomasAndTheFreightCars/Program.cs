c49596f3b3225d964393c0b2435719b36a2e33f2
wowbios <wowbios@yandex.ru>
2022-10-29 00:24:51 +0300

    Thomas and the freight cars

---
+++ ThomasAndTheFreightCars/Program.cs
@@ -0,0 +1,13 @@
+﻿using ThomasAndTheFreightCars;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.ReadLine();
+        string input = Console.ReadLine()!;
+        Console.Error.WriteLine(input);
+        int maxLength = Thomas.Solve(input);
+        Console.WriteLine(maxLength);
+    }
+}
---
+++ ThomasAndTheFreightCars/Thomas.cs
@@ -0,0 +1,76 @@
+﻿namespace ThomasAndTheFreightCars;
+
+public class Thomas
+{
+    public static int Solve(string input)
+    {
+        int[] cars = input.Split(' ').Select(int.Parse).ToArray();
+        return GetMaxTrainLength(cars);
+    }
+
+    static int GetMaxTrainLength(int[] cars)
+    {
+        BuildChildren(
+            int.MinValue,
+            int.MaxValue,
+            int.MaxValue,
+            int.MinValue,
+            0,
+            cars,
+            0);
+        return max;
+    }
+
+    private static int max = 0;
+    static void BuildChildren(
+        int front,
+        int rear,
+        int sfront,
+        int srear,
+        int length,
+        int[] cars,
+        int i)
+    {
+        if (i == cars.Length || (length + cars.Length - i) < max)
+        {
+            if (length > max)
+                max = length;
+            return;
+        }
+
+        var car = cars[i];
+        // front
+        if (front < car && car > srear)
+        {
+            BuildChildren(
+                car,
+                rear,
+                Math.Min(sfront, car),
+                srear,
+                length + 1,
+                cars,
+                i + 1);
+        }
+
+        // rear
+        if (rear > car && car < sfront)
+        {
+            BuildChildren(front,
+                car,
+                sfront,
+                Math.Max(srear, car),
+                length + 1,
+                cars,
+                i + 1);
+        }
+
+        // skip
+        BuildChildren(front,
+            rear,
+            sfront,
+            srear,
+            length,
+            cars,
+            i + 1);
+    }
+}
