bd43418ded7f4285e9f5c46de32cef1f56e9667c
wowbios <wowbios@yandex.ru>
2022-11-05 15:01:34 +0300

    tests

---
+++ Tests/HiddenFortressTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using TheHiddenFortress;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class HiddenFortressTests
+{
+    private readonly ITestOutputHelper _outputHelper;
+
+    public HiddenFortressTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int size, string field, bool[,] expected)
+    {
+        var fortress = new HiddenFortress(size, field);
+        var sw = Stopwatch.StartNew();
+        var result = fortress.Solve();
+        _outputHelper.WriteLine(sw.Elapsed.ToString());
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> TestData
+    {
+        get
+        {
+            yield return new object[]
+            {
+                3,
+                @"212
+322
+201",
+                new[,]
+                {
+                    { true, false, false },
+                    { true, false, true },
+                    { false, false, false }
+                }
+            };
+
+            yield return new object[]
+            {
+                3,
+                @"122
+212
+221",
+                new[,]
+                {
+                    { true, false, false },
+                    { false, true, false },
+                    { false, false, true }
+                }
+            };
+
+            yield return new object[]
+            {
+                5,
+                @"65756
+45655
+43545
+22423
+56756",
+                new [,]
+                {
+                    { false, true, true, true, true },
+                    { true, false, true, false, true },
+                    { false, true, true, false, false },
+                    { false, false, false, false, false },
+                    { true, false, true, true, true }
+                }
+            };
+        }
+    }
+}
---
+++ TheHiddenFortress/HiddenFortress.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+
+namespace TheHiddenFortress;
+
+public class HiddenFortress
+{
+    private readonly int _size;
+    private readonly int[,] _arr;
+
+    public HiddenFortress(int size, string field)
+    {
+        _size = size;
+        _arr = new int[size, size];
+        var fieldRows = field
+            .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < fieldRows.Length; i++)
+        {
+            for (int j = 0; j < fieldRows[i].Length; j++)
+                _arr[i, j] = int.Parse(fieldRows[i][j].ToString());
+        }
+    }
+
+    public bool[,] Solve()
+    {
+        bool[,] result = new bool[_size, _size];
+        for (int i = 0; i < _size; i++)
+        {
+            for (int j = 0; j < _size; j++)
+                result[i, j] = true;
+        }
+
+        var sw = Stopwatch.StartNew();
+        bool ok = Guess(result, _arr, 0, 0);
+        Console.WriteLine(sw.Elapsed);
+        if (!ok)
+            Console.WriteLine("Fail");
+        return result;
+    }
+
+    private bool Guess(bool[,] field, int[,] expected, int x, int y)
+    {
+        (int xx, int yy)? next = GetNext(x, y);
+        if (next is null) // end
+        {
+            if (!Validate(field, expected))
+            {
+                field[x, y] = false;
+                if (!Validate(field, expected))
+                {
+                    field[x, y] = true;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        (int xx, int yy) = next.Value;
+
+        field[x, y] = false;
+        if (!Guess(field, expected, xx, yy))
+        {
+            field[x, y] = true;
+            if (!Guess(field, expected, xx, yy))
+                return false;
+        }
+
+        return true;
+    }
+
+    private (int, int)? GetNext(int x, int y)
+    {
+        int last = _size - 1;
+        if (x == last && y == last)
+            return null;
+        if (y == last)
+            return (x + 1, 0);
+
+        return (x, y + 1);
+    }
+
+    private bool Validate(bool[,] field, int[,] expected)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            for (int j = 0; j < _size; j++)
+            {
+                int total = 0;
+                for (int c = 0; c < _size; c++)
+                {
+                    if (field[i, c]) total++;
+                    if (field[c, j]) total++;
+                }
+
+                int result = field[i, j] ? total - 1 : total;
+                if (result != expected[i, j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void Print(bool[,] next)
+    {
+        // print result
+        for (int i = 0; i < next.GetLength(0); i++)
+        {
+            for (int j = 0; j < next.GetLength(1); j++)
+            {
+                Console.Write((next[i, j] ? 'O' : '.') + "\t\t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
